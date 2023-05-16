bfa8d6475a217a154313edac521f399e36c1178e
AhmedAlfahal <ahmednader02@gmail.com>
2023-05-16 07:42:30 -0400

    working on it

--- CPP Module 01/ex04/main.cpp
+++ CPP Module 01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aalfahal <aalfahal@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/14 15:11:49 by aalfahal          #+#    #+#             */
-/*   Updated: 2023/05/14 16:02:10 by aalfahal         ###   ########.fr       */
+/*   Updated: 2023/05/16 07:42:05 by aalfahal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,26 @@
 #include <fstream>
 #include <string>
 
+std::string	replaceWords(std::string line, std::string oldWord, std::string newWord)
+{
+	std::string newLine;
+	for(size_t i = 0; i < line.length(); i++)
+	{
+		newLine[i] = line[i];
+	}
+	return (newLine);
+}
+
 bool	replcaingFile(char **av)
 {
+	std::string newFile(av[1]);
+	newFile = newFile + ".replace";
 	std::string s;
-	std::ifstream myFile; 
+	std::ifstream myFile;
 	myFile.open(av[1]);
 	if (myFile.is_open() == false)
 		return (false);
-	std::ofstream outfile (".replace");
+	std::ofstream outfile (newFile.c_str());
 	while (myFile.good())
 	{
 		std::getline(myFile, s);
