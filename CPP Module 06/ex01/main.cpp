c77436a437bd6e7744baa2cfbcd4ab06e74b3556
AhmedAlfahal <ahmednader02@gmail.com>
2023-12-18 14:10:49 +0000

    ex00 need to redo it wrong casting

--- CPP Module 06/ex01/main.cpp
+++ CPP Module 06/ex01/main.cpp
@@ -1,28 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aalfahal <aalfahal@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/17 14:13:33 by aalfahal          #+#    #+#             */
-/*   Updated: 2023/12/18 13:39:20 by aalfahal         ###   ########.fr       */
+/*   Created: 2023/12/18 13:41:17 by aalfahal          #+#    #+#             */
+/*   Updated: 2023/12/18 13:41:18 by aalfahal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SCALARCONVERTER_HPP
-#define SCALARCONVERTER_HPP
-
-#include <cstdlib>
-#include <iostream>
-
-class ScalarConverter{
-	public:
-		ScalarConverter();
-		ScalarConverter( const ScalarConverter & aScalarConverter );
-		ScalarConverter & operator= ( const ScalarConverter & aScalarConverter );	
-		virtual ~ScalarConverter() = 0;
-		static void convert( std::string aString );
-};
-
-#endif
