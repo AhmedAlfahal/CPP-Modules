62472ef9a6389b112a5b75b4dc47617705c8f137
AhmedAlfahal <ahmednader02@gmail.com>
2024-02-19 00:38:07 +0400

    fix: cpp08 ex00 c++11 removed

--- CPP Module 08/ex00/main.cpp
+++ CPP Module 08/ex00/main.cpp
@@ -13,20 +13,16 @@
 #include <exception>
 #include <vector>
 #include <deque>
-#include <forward_list>
 #include <list>
-#include <array>
 #include "easyfind.hpp"
 
 
 
 int main( void ){
 	/********************Declearing*Containers*****************/
-	std::array <int, 4> g0 = {1, 2, 3, 4};
 	std::vector<int> g1;
 	std::deque<int> g2;
-	std::forward_list<int> g3;
-	std::list<int> g4;
+	std::list<int> g3;
 	/**********************************************************/
 	/********************Inserting*Them************************/
 	for (int i = 1; i < 5; i++){
@@ -35,26 +31,19 @@ int main( void ){
 	for (int i = 1; i < 5; i++){
 		g2.push_back(i);
 	}
-	for (int i = 4; i > 0; i--){
-		g3.push_front(i);
-	}
 	for (int i = 1; i < 5; i++){
-		g4.push_back(i);
+		g3.push_back(i);
 	}
 	/**********************************************************/
 	/*******************Printing*Them**************************/
-	printCon(g0);
 	printCon(g1);
 	printCon(g2);
 	printCon(g3);
-	printCon(g4);
 	/**********************************************************/
 	try{
-		easyfind(g0, 4);
 		easyfind(g1, 3);
 		easyfind(g2, 3);
 		easyfind(g3, 3);
-		easyfind(g4, 3);
 	}
 	catch (std::exception &e){
 		std::cout << e.what() << std::endl;
